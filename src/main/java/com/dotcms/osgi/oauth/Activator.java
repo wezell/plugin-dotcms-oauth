4dff425e9ee489f996cbac8c5facec324ee12417
Will Ezell <will@dotcms.com>
2014-08-11 12:08:59 -0400

    added pass if ?native=true is in the parameters

--- src/main/java/com/dotcms/osgi/oauth/Activator.java
+++ src/main/java/com/dotcms/osgi/oauth/Activator.java
@@ -7,6 +7,7 @@ import org.apache.felix.http.api.ExtHttpService;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.osgi.util.tracker.ServiceTracker;
+import org.tuckey.web.filters.urlrewrite.Condition;
 import org.tuckey.web.filters.urlrewrite.NormalRule;
 import org.tuckey.web.filters.urlrewrite.Rule;
 
@@ -52,32 +53,75 @@ public class Activator extends GenericBundleActivator {
 		serviceTracker.open();
 
 		NormalRule rule = new NormalRule();
+		
+		
+        //Create Conditions for this rule
+        Condition condition1 = new Condition();
 
+		condition1 = new Condition();
+        condition1.setName( "native" );
+        condition1.setType("parameter");
+        condition1.setOperator("notequal");
+        condition1.setValue( "^.+$" );
+        
+        //Create another Condition for this rule
+        Condition condition2 = new Condition();
+        condition2.setName( "my_account_r_m" );
+        condition2.setType("parameter");
+        condition2.setOperator("notequal");
+        condition2.setValue( "^.+$" );
+
+        
+        
+        rule = new NormalRule();
 		rule.setName("oauth-rule" + rules.size());
 		rule.setFrom("^/dotCMS/login.*$");
 		rule.setTo("/app" + OAUTH_URL);
+		rule.addCondition(condition1);
+		rule.addCondition(condition2);
 		addRewriteRule(rule);
 		rules.add(rule);
+		
 		
 		
 		rule = new NormalRule();
 		rule.setName("oauth-rule" + rules.size());
 		rule.setFrom("^/html/portal/login.*$");
 		rule.setTo("/app" + OAUTH_URL + "?referrer=/c/portal/layout");
+		rule.addCondition(condition1);
+		rule.addCondition(condition2);
 		addRewriteRule(rule);
 		rules.add(rule);
+		
+		
+		rule = new NormalRule();
+		rule.setName("oauth-rule" + rules.size());
+		rule.setFrom("^/c/public/login.*$");
+		rule.setTo("/app" + OAUTH_URL + "?referrer=/c/portal/layout");
+		rule.addCondition(condition1);
+		rule.addCondition(condition2);
+		addRewriteRule(rule);
+		rules.add(rule);
+		
 		
 		rule = new NormalRule();
 		rule.setName("oauth-rule" + rules.size());
 		rule.setFrom("^/c/portal_public/login.*$");
 		rule.setTo("/app" + OAUTH_URL + "?referrer=/c/portal/layout");
+		rule.addCondition(condition1);
+		rule.addCondition(condition2);
 		addRewriteRule(rule);
 		rules.add(rule);
+		
+		
+		
 		
 		rule = new NormalRule();
 		rule.setName("oauth-rule" + rules.size());
 		rule.setFrom("^/c/portal/logout.*$");
 		rule.setTo("/c/portal/logout?referer=/");
+		rule.addCondition(condition1);
+		rule.addCondition(condition2);
 		addRewriteRule(rule);
 		rules.add(rule);
 		
